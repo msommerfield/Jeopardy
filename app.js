b91d0018597562bb9e13a9e8bca23cb42aa026ce
Mandy <mandysommerfield@gmail.com>
2019-02-15 14:12:23 -0500

    psudocoded and added a seperate file for functions

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log('hola')
 
 //prompt(whats your name?) assign to a button
+//create a start button and have it begin the game
+//connect player one totheir score and player two to their score
+//use modals to assign answers & questions 
+//include the content in the model on the main page
