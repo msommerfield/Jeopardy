e938f1500a77775b44c928acea8479b9a7a6a316
Mandy <mandysommerfield@gmail.com>
2019-02-15 14:22:50 -0500

    added modal to meta data for content

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ console.log('hola')
 
 //prompt(whats your name?) assign to a button
 //create a start button and have it begin the game
-//connect player one totheir score and player two to their score
+//connect player one to their score and player two to their score
 //use modals to assign answers & questions 
+$("#fade").modal({
+    fadeDuration: 100
+  });
 //include the content in the model on the main page
